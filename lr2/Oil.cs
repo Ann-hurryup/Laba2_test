42c78f43760c1a334647d16dd068598284a925fe
Anna Pankova <108939832+Ann-hurryup@users.noreply.github.com>
2024-04-22 23:00:01 +0400

    oil

--- lr2/Oil.cs
+++ lr2/Oil.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace lr2
 {
-    internal class Oil
+    public class Oil
     {
+        public Chemist Chemist { get; set; }
+        public Transport Transport { get; set; }
+        public bool IsOilTest { get; set; }
+        public bool IsOilTransport { get; set; }
+
+        public Oil(Chemist chemist, Transport transport)
+        {
+            Chemist = chemist;
+            Transport = transport;
+        }
+
+        public void TransportOil(Transport transport)
+        {
+            IsOilTransport = true;
+            Console.WriteLine($"Нефть транспортируется с помощью {transport.IdTransport}");
+        }
+
+        public void NotTransportOil(Transport transport)
+        {
+            IsOilTransport = false;
+            Console.WriteLine("Нефть не транспортируется");
+        }
+
+        public void AnalyseOil(Chemist chemist)
+        {
+            IsOilTest = true;
+            Console.WriteLine($"Нефть анализируется химиком {chemist.FullNameCh}");
+        }
+
+        public void NotAnalyseOil(Chemist chemist)
+        {
+            IsOilTest = false;
+            Console.WriteLine("Нефть не анализируется");
+        }
     }
 }
